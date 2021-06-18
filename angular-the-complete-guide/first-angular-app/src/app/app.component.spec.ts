63d9659856e3747f4905516b7ee26e53f1430b56
SoftwareBlair <br@ndon.dev>
2021-06-17 23:09:53 -0600

    Added server and servers components

--- angular-the-complete-guide/first-angular-app/src/app/app.component.spec.ts
+++ angular-the-complete-guide/first-angular-app/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'first-angular-app'`, () => {
+  it(`should have as appComponent 'first-angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('first-angular-app');
+    expect(app.appComponent).toEqual('App Component');
   });
 
-  it('should render title', () => {
+  it('should render appComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('first-angular-app app is running!');
+    expect(compiled.querySelector('h3').textContent).toContain('App Component');
   });
 });
